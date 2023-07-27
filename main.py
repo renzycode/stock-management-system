f1e28800139412797d8fde2cc2d3a4b0c551b94d
renzycode <renzyjohnm1@gmail.com>
2023-07-27 19:45:53 +0800

    updated

--- main.py
+++ main.py
@@ -15,17 +15,17 @@ ph3 = tk.StringVar()
 ph4 = tk.StringVar()
 ph5 = tk.StringVar()
 
-MyFontArray = ['Arial', 15]
+myFontArray = ['Arial', 15]
 
-label = Label(root, text="Stock Management System", font=(MyFontArray))
+label = Label(root, text="Stock Management System", font=(myFontArray))
 label.grid(row=0, column=0, columnspan=8, rowspan=2, padx=50, pady=40)
 
-itemNoLabel = Label(root, text="Item No.",anchor="e",width=10, font=(MyFontArray))
+itemNoLabel = Label(root, text="Item No.",anchor="e",width=10, font=(myFontArray))
 
-nameLabel = Label(root, text="Name",anchor="e",width=10, font=(MyFontArray))
-categoryLabel = Label(root, text="Category",anchor="e",width=10, font=(MyFontArray))
-priceLabel = Label(root, text="Price",anchor="e",width=10, font=(MyFontArray))
-quantityLabel = Label(root, text="Quantity",anchor="e",width=10, font=(MyFontArray))
+nameLabel = Label(root, text="Name",anchor="e",width=10, font=(myFontArray))
+categoryLabel = Label(root, text="Category",anchor="e",width=10, font=(myFontArray))
+priceLabel = Label(root, text="Price",anchor="e",width=10, font=(myFontArray))
+quantityLabel = Label(root, text="Quantity",anchor="e",width=10, font=(myFontArray))
 
 itemNoLabel.grid(row=3, column=0, columnspan=1, padx=50, pady=5)
 
@@ -34,15 +34,15 @@ categoryLabel.grid(row=5, column=0, columnspan=1, padx=50, pady=5)
 priceLabel.grid(row=6, column=0, columnspan=1, padx=50, pady=5)
 quantityLabel.grid(row=7, column=0, columnspan=1, padx=50, pady=5)
 
-itenNoEntry = Entry(root, width=40, bd=5, font=(MyFontArray), textvariable = ph1)
+itenNoEntry = Entry(root, width=40, bd=5, font=(myFontArray), state='disabled', textvariable = ph1)
 generateNoBtn = Button(
-    root, text="Generate No.", padx=20, pady=1, width=10,
-    bd=5, font=(MyFontArray), bg="#84F894")
+    root, text="Generate No.", padx=20, pady=1, width=9,
+    bd=5, font=(myFontArray), bg="#d7eeb4")
 
-nameEntry = Entry(root, width=55, bd=5, font=(MyFontArray), textvariable = ph2)
-categoryEntry = Entry(root, width=55, bd=5, font=(MyFontArray), textvariable = ph3)
-priceEntry = Entry(root, width=55, bd=5, font=(MyFontArray), textvariable = ph4)
-quantityEntry = Entry(root, width=55, bd=5, font=(MyFontArray), textvariable = ph5)
+nameEntry = Entry(root, width=55, bd=5, font=(myFontArray), textvariable = ph2)
+categoryEntry = Entry(root, width=55, bd=5, font=(myFontArray), textvariable = ph3)
+priceEntry = Entry(root, width=55, bd=5, font=(myFontArray), textvariable = ph4)
+quantityEntry = Entry(root, width=55, bd=5, font=(myFontArray), textvariable = ph5)
 
 itenNoEntry.grid(row=3, column=1, columnspan=3, padx=5, pady=0)
 generateNoBtn.grid(row=3, column=4, columnspan=1, rowspan=1)
@@ -51,5 +51,31 @@ nameEntry.grid(row=4, column=1, columnspan=4, padx=5, pady=0)
 categoryEntry.grid(row=5, column=1, columnspan=4, padx=5, pady=0)
 priceEntry.grid(row=6, column=1, columnspan=4, padx=5, pady=0)
 quantityEntry.grid(row=7, column=1, columnspan=4, padx=5, pady=0)
+
+addBtn = Button(
+    root, text="Select", padx=50, pady=25, width=10,
+    bd=5, font=(myFontArray), bg="#84F894")
+updateBtn = Button(
+    root, text="Find", padx=50, pady=25, width=10,
+    bd=5, font=(myFontArray), bg="#84E8F8")
+deleteBtn = Button(
+    root, text="Save", padx=50, pady=25, width=10,
+    bd=5, font=(myFontArray), bg="#FF9999")
+searchBtn = Button(
+    root, text="Update", padx=50, pady=25, width=10,
+    bd=5, font=(myFontArray), bg="#F4FE82")
+resetBtn = Button(
+    root, text="Delete", padx=50, pady=25, width=10,
+    bd=5, font=(myFontArray), bg="#F398FF")
+selectBtn = Button(
+    root, text="Reset", padx=50, pady=25, width=10,
+    bd=5, font=(myFontArray), bg="#EEEEEE")
+
+addBtn.grid(row=3, column=5, columnspan=1, rowspan=2)
+updateBtn.grid(row=5, column=5, columnspan=1, rowspan=2)
+deleteBtn.grid(row=7, column=5, columnspan=1, rowspan=2)
+searchBtn.grid(row=9, column=5, columnspan=1, rowspan=2)
+resetBtn.grid(row=11, column=5, columnspan=1, rowspan=2)
+selectBtn.grid(row=13, column=5, columnspan=1, rowspan=2)
 
 root.mainloop()
