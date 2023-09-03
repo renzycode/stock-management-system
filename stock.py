702809dab21b50c6cfc28df0725ef831d02524d2
renzycode <renzyjohnm1@gmail.com>
2023-09-04 02:00:54 +0800

    update

--- stock.py
+++ stock.py
@@ -22,7 +22,7 @@ def connection():
         host='localhost',
         user='root', 
         password='',
-        db='stockmanagement',
+        db='stockmanagementsystem',
     )
     return conn
 
@@ -41,7 +41,7 @@ def read():
     conn = connection()
     cursor = conn.cursor()
     cursor.connection.ping()
-    cursor.execute("SELECT `item_id`,`name`,`price`,`qnt`,`category`,`date` FROM stocks ORDER BY `id` DESC")
+    cursor.execute("SELECT `item_id`,`name`,`price`,`quantity`,`category`,`date` FROM stocks ORDER BY `id` DESC")
     results = cursor.fetchall()
     conn.commit()
     conn.close()
@@ -49,7 +49,7 @@ def read():
 
 def exportExcel():
     cursor.connection.ping()
-    cursor.execute("SELECT `item_id`,`name`,`price`,`qnt`,`category`,`date` FROM stocks ORDER BY `id` DESC")
+    cursor.execute("SELECT `item_id`,`name`,`price`,`quantity`,`category`,`date` FROM stocks ORDER BY `id` DESC")
     dataraw = cursor.fetchall()
     date = str(datetime.now());
     date = date.replace(' ', '_')
@@ -88,7 +88,7 @@ def save():
     itemId = str(itemIdEntry.get())
     name = str(nameEntry.get())
     price = str(priceEntry.get())
-    qnt = str(qntEntry.get())
+    quantity = str(quantityEntry.get())
     cat = str(categoryCombo.get())
     valid = True
     if(not(itemId[3]=='-')):
@@ -102,7 +102,7 @@ def save():
     if not(valid):
         messagebox.showwarning("", "Invalid Item Id")
         return
-    if (not(itemId and itemId.strip())) or (not(name and name.strip())) or (not(price and price.strip())) or (not(qnt and qnt.strip())) or (not(cat and cat.strip())):
+    if (not(itemId and itemId.strip())) or (not(name and name.strip())) or (not(price and price.strip())) or (not(quantity and quantity.strip())) or (not(cat and cat.strip())):
         messagebox.showwarning("", "Please fill up all entries")
         return
     else:
@@ -115,7 +115,7 @@ def save():
                 messagebox.showwarning("", "Item Id already used")
             else:
                 cursor.connection.ping()
-                sql2=f"INSERT INTO stocks (`item_id`, `name`, `price`, `qnt`, `category`) VALUES ('{itemId}','{name}','{price}','{qnt}','{cat}') "
+                sql2=f"INSERT INTO stocks (`item_id`, `name`, `price`, `quantity`, `category`) VALUES ('{itemId}','{name}','{price}','{quantity}','{cat}') "
                 cursor.execute(sql2)
             conn.commit()
             conn.close()
@@ -138,9 +138,9 @@ def update():
     itemId = str(itemIdEntry.get())
     name = str(nameEntry.get())
     price = str(priceEntry.get())
-    qnt = str(qntEntry.get())
+    quantity = str(quantityEntry.get())
     cat = str(categoryCombo.get())
-    if (not(itemId and itemId.strip())) or (not(name and name.strip())) or (not(price and price.strip())) or (not(qnt and qnt.strip())) or (not(cat and cat.strip())):
+    if (not(itemId and itemId.strip())) or (not(name and name.strip())) or (not(price and price.strip())) or (not(quantity and quantity.strip())) or (not(cat and cat.strip())):
         messagebox.showinfo("Error", "Please fill up the blank entry")
         return
     if(selectedItemid!=itemId):
@@ -149,7 +149,7 @@ def update():
     else:
         try:
             cursor.connection.ping()
-            sql=f"UPDATE stocks SET `name` = '{name}', `price` = '{price}', `qnt` = '{qnt}', `category` = '{cat}' WHERE `item_id` = '{itemId}' "
+            sql=f"UPDATE stocks SET `name` = '{name}', `price` = '{price}', `quantity` = '{quantity}', `category` = '{cat}' WHERE `item_id` = '{itemId}' "
             cursor.execute(sql)
             conn.commit()
             conn.close()
@@ -188,12 +188,12 @@ def select():
         itemId = str(my_tree.item(selected_item)['values'][0])
         name = str(my_tree.item(selected_item)['values'][1])
         price = str(my_tree.item(selected_item)['values'][2])
-        qnt = str(my_tree.item(selected_item)['values'][3])
+        quantity = str(my_tree.item(selected_item)['values'][3])
         category = str(my_tree.item(selected_item)['values'][4])
         setph(itemId,0)
         setph(name,1)
         setph(price,2)
-        setph(qnt,3)
+        setph(quantity,3)
         setph(category,4)
     except:
         messagebox.showwarning("", "Please select a data row")
@@ -202,19 +202,19 @@ def find():
     itemId = str(itemIdEntry.get())
     name = str(nameEntry.get())
     price = str(priceEntry.get())
-    qnt = str(qntEntry.get())
+    quantity = str(quantityEntry.get())
     category = str(categoryCombo.get())
     cursor.connection.ping()
     if(itemId and itemId.strip()):
-        sql = f"SELECT `item_id`,`name`,`price`,`qnt`,`category` FROM stocks WHERE `item_id` LIKE '%{itemId}%' "
+        sql = f"SELECT `item_id`,`name`,`price`,`quantity`,`category` FROM stocks WHERE `item_id` LIKE '%{itemId}%' "
     elif(name and name.strip()):
-        sql = f"SELECT `item_id`,`name`,`price`,`qnt`,`category` FROM stocks WHERE `name` LIKE '%{name}%' "
+        sql = f"SELECT `item_id`,`name`,`price`,`quantity`,`category` FROM stocks WHERE `name` LIKE '%{name}%' "
     elif(price and price.strip()):
-        sql = f"SELECT `item_id`,`name`,`price`,`qnt`,`category` FROM stocks WHERE `price` LIKE '%{price}%' "
-    elif(qnt and qnt.strip()):
-        sql = f"SELECT `item_id`,`name`,`price`,`qnt`,`category` FROM stocks WHERE `qnt` LIKE '%{qnt}%' "
+        sql = f"SELECT `item_id`,`name`,`price`,`quantity`,`category` FROM stocks WHERE `price` LIKE '%{price}%' "
+    elif(quantity and quantity.strip()):
+        sql = f"SELECT `item_id`,`name`,`price`,`quantity`,`category` FROM stocks WHERE `quantity` LIKE '%{quantity}%' "
     elif(category and category.strip()):
-        sql = f"SELECT `item_id`,`name`,`price`,`qnt`,`category` FROM stocks WHERE `category` LIKE '%{category}%' "
+        sql = f"SELECT `item_id`,`name`,`price`,`quantity`,`category` FROM stocks WHERE `category` LIKE '%{category}%' "
     else:
         messagebox.showwarning("", "Please fill up one on the entries")
         return
@@ -264,8 +264,8 @@ nameLabel = Label(entriesFrame, text="NAME",anchor="e",width=10)
 nameLabel.grid(row=1, column=0,padx=10)
 priceLabel = Label(entriesFrame, text="PRICE",anchor="e",width=10)
 priceLabel.grid(row=2, column=0,padx=10)
-qntLabel = Label(entriesFrame, text="QUANTITY",anchor="e",width=10)
-qntLabel.grid(row=3, column=0,padx=10)
+quantityLabel = Label(entriesFrame, text="QUANTITY",anchor="e",width=10)
+quantityLabel.grid(row=3, column=0,padx=10)
 categoryLabel = Label(entriesFrame, text="CATEGORY",anchor="e",width=10)
 categoryLabel.grid(row=4, column=0,padx=10)
 
@@ -275,8 +275,8 @@ nameEntry = Entry(entriesFrame, width=50,textvariable = placeholderArray[1])
 nameEntry.grid(row=1,column=2,padx=5,pady=5)
 priceEntry = Entry(entriesFrame, width=50,textvariable = placeholderArray[2])
 priceEntry.grid(row=2,column=2,padx=5,pady=5)
-qntEntry = Entry(entriesFrame, width=50,textvariable = placeholderArray[3])
-qntEntry.grid(row=3,column=2,padx=5,pady=5)
+quantityEntry = Entry(entriesFrame, width=50,textvariable = placeholderArray[3])
+quantityEntry.grid(row=3,column=2,padx=5,pady=5)
 categoryCombo = ttk.Combobox(entriesFrame, values=['Networking Tools','Computer Parts','Repair Tools','Gadgets'], width=47,textvariable = placeholderArray[4])
 categoryCombo.grid(row=4,column=2,padx=5,pady=5)
 
